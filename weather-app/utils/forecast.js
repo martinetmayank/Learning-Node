4a0f1381b8b89cf8b38dd177656aabec73c526ed
Mayank Garg <mayank.captain00@gmail.com>
2019-11-10 02:06:36 +0530

    [Updated] ES6 and short-hand Notation.

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,17 +1,18 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/60a53dcbd37ae5c980e589d32b6aeeab/'+ latitude + ',' + longitude +'?units=si'
+    const url = 'https://api.darksky.net/forecast/60a53dcbd37ae5c980e589d32b6aeeab/' + latitude + ',' + longitude + '?units=si'
 
-    request({url: url, json:true}, (error, response) => {
+    // Response is destructured into { body }.
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather services!', undefined)
         }
-        else if (response.body.error) {
+        else if (body.error) {
             callback('Unable to find location', undefined)
         }
         else {
-            callback(undefined, response.body.daily.data[0].summary + ' Temperature: ' + response.body.currently.temperature + '°C')
+            callback(undefined, body.daily.data[0].summary + ' Temperature: ' + body.currently.temperature + '°C')
         }
     })
 }
